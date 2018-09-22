8f62871e88b7bcd830c398f08b614472bd164420
Nasonov V <qxuken@gmail.com>
2018-09-22 13:51:39 +0300

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module WaspaidBackend
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :options]
+      end
+    end
+  end
+end
