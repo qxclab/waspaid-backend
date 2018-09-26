76fbb53cd306d0d6772f5dc30b60fbd3810a8d84
Nasonov V <qxuken@gmail.com>
2018-09-27 02:57:20 +0300

    tests for credits

--- spec/fabricators/credit_fabricator.rb
+++ spec/fabricators/credit_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:credit) do
   description {Faker::Lovecraft.sentence}
   value {Faker::Number.decimal(2)}
   fee {Faker::Number.decimal(2)}
-  expired_at {DateTime.now}
+  expired_at {DateTime.tomorrow}
   author {Fabricate(:user)}
   issued {Fabricate(:user)}
 end
